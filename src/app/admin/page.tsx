821c89941c06fc452484727d2a60d4ec84410ae9
Veyzaputri <129712217+Veyzaputri@users.noreply.github.com>
2025-10-07 15:12:15 +0700

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,11 @@
 "use server";
+import DashboardLayout from "@/components/organisms/DashboardLayout";
+import AdminManagementPage from "@/components/organisms/AdminManagementPage";
 
-export default async function AdminPage() {
-  return "Admin Page";
+export default async function AdminPages() {
+  return (
+    <DashboardLayout>
+      <AdminManagementPage />
+    </DashboardLayout>
+  );
 }
