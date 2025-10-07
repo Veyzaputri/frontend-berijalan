c514c76f3c135ff93587a3bb22faaca5ba463367
Veyzaputri <129712217+Veyzaputri@users.noreply.github.com>
2025-10-07 15:26:28 +0700

    Update counter.interface.ts

--- src/interfaces/service/counter.interface.ts
+++ src/interfaces/service/counter.interface.ts
@@ -1,10 +1,11 @@
 import CurrentQueueDisplay from '../../components/molecules/CurrentQueueDisplay';
+import { ActiveQueue } from './queue.interface';
 export interface ICounter {
     id: number
     name: string
     CurrentQueue: number
     maxQueue: number
-    isActive: boolean
+    isActive?: boolean
     createdAt: string
     updateAt: string
     deletedAt: string | null
@@ -16,8 +17,11 @@ export interface ICreateCounterRequest {
     is_active: boolean
 }
 
-export interface IUpdateCounterRequest extends ICreateCounterRequest {
-    id?: number
+export interface IUpdateCounterRequest {
+    name?: string
+    max_queue?: number
+    is_active?: boolean
+    currentQueue?: number   // <--- tambahkan ini
 }
 
 export interface ICounterResponse {
@@ -30,3 +34,4 @@ export interface ICounterResponse {
     updateAt: string
     deletedAt: string | null
 }
+
