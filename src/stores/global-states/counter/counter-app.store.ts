4cf30fdb0f1c403443a2a597e52ce0aa53085f38
Veyzaputri <129712217+Veyzaputri@users.noreply.github.com>
2025-10-07 15:31:17 +0700

    Update counter-app.store.ts

--- src/stores/global-states/counter/counter-app.store.ts
+++ src/stores/global-states/counter/counter-app.store.ts
@@ -1,43 +1,52 @@
-import { ICounter } from '@/interfaces/service/counter.interface'
-import { IQueue } from '@/interfaces/service/queue.interface'
-import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
+import { ICounter } from '@/interfaces/service/counter.interface';
+import { IQueue } from '@/interfaces/service/queue.interface';
+import { create, StateCreator } from 'zustand';
+import { persist, PersistOptions } from 'zustand/middleware';
 
 type InitialState = {
-    selectedCounter: ICounter | null
-    currentQueue: IQueue| null
-    claimedQueue: {
-        queueNumber: number
-        estimatedWaitTime: number
-        position: number
-        counterName: string
-        counterId: number
-    } | null
-}
+  counters: ICounter[];
+  selectedCounter: ICounter | null;
+  currentQueue: IQueue | null;
+  claimedQueue: {
+    queueNumber: number;
+    estimatedWaitTime: number;
+    position: number;
+    counterName: string;
+    counterId: number;
+  } | null;
+};
 
 type ICounterAppStore = InitialState & {
-    setSelectedCounter: (counter: InitialState['selectedCounter']) => void
-    setCurrentQueue: (queue: InitialState['currentQueue']) => void
-    setClaimedQueue: (queue: InitialState['claimedQueue']) => void
-    reset: () => void
-}   
+  setCounters: (counters: ICounter[]) => void;
+  setSelectedCounter: (counter: InitialState['selectedCounter']) => void;
+  setCurrentQueue: (queue: InitialState['currentQueue']) => void;
+  setClaimedQueue: (queue: InitialState['claimedQueue']) => void;
+  reset: () => void;
+};
+
+// Generic helper untuk persist + TypeScript
+type MyPersist = <T>(
+  config: StateCreator<T>,
+  options: PersistOptions<T>
+) => StateCreator<T>;
 
 const initialState: InitialState = {
-    selectedCounter: null,
-    currentQueue: null,
-    claimedQueue:null,
+  counters: [],
+  selectedCounter: null,
+  currentQueue: null,
+  claimedQueue: null,
+};
 
-}
-
-export const useCounterAppStore = create(
-    persist<ICounterAppStore>(
-        set => ({
-            ...initialState,
-            setSelectedCounter: selectedCounter => set ({ selectedCounter}),
-            setCurrentQueue: currentQueue => set({ currentQueue}),
-            setClaimedQueue: claimedQueue => set({ claimedQueue}),
-            reset: () => set(initialState),
-        }),
-        {name: 'counter-app-store'}
-    )
-)
+export const useCounterAppStore = create<ICounterAppStore>(
+  (persist as MyPersist)(
+    (set) => ({
+      ...initialState,
+      setCounters: (counters) => set({ counters }),
+      setSelectedCounter: (selectedCounter) => set({ selectedCounter }),
+      setCurrentQueue: (currentQueue) => set({ currentQueue }),
+      setClaimedQueue: (claimedQueue) => set({ claimedQueue }),
+      reset: () => set(initialState),
+    }),
+    { name: 'counter-app-store' }
+  )
+);
