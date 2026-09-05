0459274c96e259151a288f70aeafd26f887f24b8
Veyzaputri <129712217+Veyzaputri@users.noreply.github.com>
2026-09-05 15:00:53 +0700

    Update page.tsx

--- src/app/queue-status/page.tsx
+++ src/app/queue-status/page.tsx
@@ -1,6 +1,7 @@
-"use server";
 import DashboardLayout from "@/components/organisms/DashboardLayout";
 import QueueStatus from "@/components/organisms/QueueStatus";
+
+export const dynamic = "force-dynamic";
 
 export default async function Page() {
   return (
